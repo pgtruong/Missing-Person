5d2239aa5038f38005b3cc17caf6c40dd76c25b2
pgtruong <petergtruong@gmail.com>
2017-05-09 16:25:46 -0700

    05/09/2017 4:25 PM - Added FPS Controller, House Model, and Grass

---
+++ Missing Person/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float speed;
+
+    void Update()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        
+    }
+}
