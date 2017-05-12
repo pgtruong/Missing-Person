527b03c9dd122f06ae45960e35fbcc57185ad617
Peter Truong <petergtruong@gmail.com>
2017-05-11 22:10:04 -0700

    5/11/2017 10:00 PM - Door animated, skybox added
    Added nice lighting to the environment and the door can open and close.

---
+++ Missing Person/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+
+    Animator animator;
+
+    /// <summary>
+    /// Bool that represents whether or not the door is open.
+    /// </summary>
+    [SerializeField] private bool isOpen = false;
+    public bool IsDoorOpen
+    {
+        get { return this.isOpen; }
+        set { this.isOpen = value; }
+    }
+
+    float rotation = -90;
+    public bool playerCanOpen = false;
+    public float doorSpeed = 200f;
+    GameObject DoorFrame;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        DoorFrame = transform.FindChild("Door_Wood").gameObject;
+    }   
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerCanOpen)
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                StopAllCoroutines();
+                StartCoroutine(rotateDoor(-rotation));
+            }
+            else
+            {
+                isOpen = true;
+                StopAllCoroutines();
+                StartCoroutine(rotateDoor(rotation));
+            }            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCanOpen = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCanOpen = false;
+        }
+    }
+
+    /// <summary>
+    /// This allows the door to open or close based on the rotation since the animator was not working with this free asset
+    /// </summary>
+    /// <param name="rotate">Set how you want the door to rotate.</param>
+    /// <returns>Makes the door rotate by the rotate param.</returns>
+    IEnumerator rotateDoor(float rotate)
+    {
+        Vector3 target = new Vector3(DoorFrame.transform.localRotation.x, DoorFrame.transform.localRotation.y, rotate);
+        rotate = (rotate < 0) ? rotate + 360 : 0;
+        if (!isOpen)
+            target = new Vector3(target.x, target.y, 360);
+        while (Mathf.Abs(DoorFrame.transform.localEulerAngles.z - rotate) > 5f)
+        {
+            DoorFrame.transform.localEulerAngles = Vector3.MoveTowards(DoorFrame.transform.localEulerAngles, target, Time.deltaTime * doorSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+        DoorFrame.transform.localEulerAngles = target;
+    }
+}
--- Missing Person/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Missing Person/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
