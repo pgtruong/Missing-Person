5fea6a6119e05809f420b56e1eb456dceb53039f
pgtruong <petergtruong@gmail.com>
2017-05-18 15:52:32 -0700

    5/18/2017 3:52 PM - Added pickup objects

---
+++ Missing Person/Assets/Scripts/PickUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    private bool pickUp = false;
+    public GameObject[] dependencies;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && pickUp)
+        {
+            foreach (GameObject dependent in dependencies)
+            {
+                if (dependent != null)
+                    return;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pickUp = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pickUp = false;
+        }
+    }
+}
