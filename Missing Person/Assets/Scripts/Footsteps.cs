a858ae40f8a761b5f99541b6439bd8e4255dd754
pgtruong <petergtruong@gmail.com>
2017-05-18 15:01:10 -0700

    5/18/2017 3:00 PM - Added footsteps for grass

---
+++ Missing Person/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public float maxStepInterval;
+    private float interval;
+    Vector3 lastPos;
+    bool isWalk = false;
+
+    void Start()
+    {
+        interval = maxStepInterval;
+        lastPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (lastPos != transform.position)
+            isWalk = true;
+        else
+            isWalk = false;
+        
+        if (isWalk)
+        {
+            interval -= Time.deltaTime;
+            if (interval <= 0)
+            {
+                interval = maxStepInterval;
+                AkSoundEngine.PostEvent("Footstep", this.gameObject);
+            }
+        }
+        else        
+            interval = 0;
+        lastPos = transform.position; 
+    }
+}
